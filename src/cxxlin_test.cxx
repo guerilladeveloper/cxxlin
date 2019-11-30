5e05b0d186dfa908bddc1a886a02e455c34370ec
Benjamin Briggs <bwbriggs1980@gmail.com>
2019-11-29 21:41:07 -0800

    fixed bugs

--- src/cxxlin_test.cxx
+++ src/cxxlin_test.cxx
@@ -2,6 +2,8 @@
 #include "cxxlin_vec2.h"
 #include "cxxlin_vec3.h"
 #include "cxxlin_vecf.h"
+//#include "cxxlin_vecptr.h"
+//#include "cxxlin_vecref.h"
 #include <stdio.h>
 
 #include "cxxlin_vec2f.cxx"
@@ -20,17 +22,25 @@ int main(int argc,char* argv[])
 
 	vec<2,real32> v2f=vec<2,real32>(3.14);
 	vec<2,real32> v2f2=vec<2,real32>(5.0f,2.1f);
-	vec<5,uint32> v5u;
+	vec<4,uint32> v4u;
 
 	printf("%f,%f\n",v2f[0],v2f[1]);
 
-	vec<2,real32*> ptrVec=vec<2,real32*>(&v2f.x,&v2f.y);
+	vec<2,real32*> ptrVec;
+	ptrVec[0]=&v2f.x;
+	ptrVec[1]=&v2f.y;
 	*ptrVec[0]=1.0f;
 
 	printf("%f,%f\n",v2f[0],v2f[1]);
+	printf("%f,%f\n",*ptrVec[0],*ptrVec[1]);
 	
 	v2f=v2f+v2f2;
 
 	printf("%f,%f\n",*ptrVec[0],*ptrVec[1]);
+
+	vec<4,uint32&> uPtrVec=vec<4,uint32*>(v4u.e);
+
+	*uPtrVec[0]=50;
+	printf("%d,%d\n",v4u[0],v4u[1]);
 	return 0;
 }
